4814e46b9a20301629b1c0f799e51dbb1b4c064e
misha <shma2001@gmail.com>
2019-07-04 09:06:36 +0300

    use well-tested libs

--- src/main/java/org.misha/utils/ClassMethods.java
+++ src/main/java/org.misha/utils/ClassMethods.java
@@ -1,19 +1,19 @@
 package org.misha.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.google.common.collect.Sets;
 import com.google.gson.GsonBuilder;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.experimental.UtilityClass;
-import org.springframework.context.annotation.Bean;
+import org.springframework.util.ReflectionUtils;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -25,17 +25,10 @@ import static java.util.stream.Collectors.toSet;
 
 @UtilityClass
 public class ClassMethods {
-    private static final Set<Method> skip = new HashSet<Method>() {{
-        addAll(Arrays.asList(Object.class.getMethods()));
-    }};
+    private static final Set<Method> skip = Sets.newHashSet(Object.class.getMethods());
 
     private static Set<Method> methods(Class<?> c) {
-        final Set<Method> methods = new HashSet<>();
-        if (c.getSuperclass() != null) {
-            methods.addAll(methods(c.getSuperclass()));
-        }
-        methods.addAll(Arrays.asList(c.getDeclaredMethods()));
-        return methods.stream().filter(m -> !skip.contains(m)).collect(toSet());
+        return Arrays.stream(ReflectionUtils.getAllDeclaredMethods(c)).filter(m -> !skip.contains(m)).collect(toSet());
     }
 
     public static String describePublic(final Class<?> c) throws JsonProcessingException {
